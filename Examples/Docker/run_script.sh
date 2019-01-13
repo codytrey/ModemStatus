6c5d98af099c53cbea93f5fdada81d74723938e4
Cody Belcher <cody.t.belcher@gmail.com>
2019-01-13 17:27:02 -0600

    fixed mistakes in Dockerfile and example scripts

--- Examples/Docker/run_script.sh
+++ Examples/Docker/run_script.sh
@@ -4,11 +4,12 @@ source virtenv/bin/activate
 
 i=0
 while true; do
-    python ModemStatus/Examples/Update_All/update_all.py /tmp/modemstatus/modem.db
+    python /ModemStatus/Examples/Update_All/update_all.py /tmp/modemstatus/modem.db
     sleep $PERIOD
     if [[ $i == 5 ]]; then
-        python ModemStatus/Examples/Graph_Stats.py /tmp/modemstatus/modem.db
+        python /ModemStatus/Examples/Graph_Stats.py /tmp/modemstatus/modem.db
         i=0
     else:
-        i=$((i+1)
+        i=$(i+1)
+    fi
 done
